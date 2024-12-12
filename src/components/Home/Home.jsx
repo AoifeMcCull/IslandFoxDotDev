94937a61ffed0c12024fc963f4a1b5b66a70204f
Jamie McCullough <92225779+KeeganMcCullough@users.noreply.github.com>
2024-12-12 10:52:06 -0600

    What I Do tab finished

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -63,7 +63,19 @@ function Home(){
                         <img className='reduxLogo' src='images\redux.png' />
                         <img className='axiosLogo' src='images\axios.png' />
                     </div>
-                    
+                    <span className='verticalSpacer' />
+                    <h2 className='sectionTitle'>I build Backends.</h2>
+                    <p className='info'>I have in-industry experience at ITS, Inc.</p>
+                    <p className='info'>as a backend dev using Java, Kotlin, Spring, and MySQL.</p>
+                    <div className='logosContainer'>
+                        <img className='leftLogo' src='images\java.png' />
+                        <img className='leftLogo' src='images\kotlin.png' />
+                        <img className='rightLogo' src='images\spring.png' />
+                        <img className='rightLogo' src='images\mysql.png' />
+                    </div>
+
+                    <h2 className='sectionTitle'>I build Mobile Apps.</h2>
+                    <p className='info'>I'm building the Body Confidence 100 app for iOS and Android for my client.</p>
                 </CustomTabPanel>
                 <CustomTabPanel value={value} index={1}>
                     <h2 className='sectionTitle'>I love the Island Fox (Urocyon Littoralis)</h2>
