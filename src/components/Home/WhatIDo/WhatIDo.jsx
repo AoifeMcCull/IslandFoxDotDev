92a6c53f3fcac93e43f76dfc20fa45257d5bae39
Jamie McCullough <92225779+KeeganMcCullough@users.noreply.github.com>
2024-12-12 11:04:54 -0600

    moved images to public

--- src/components/Home/WhatIDo/WhatIDo.jsx
+++ src/components/Home/WhatIDo/WhatIDo.jsx
@@ -6,20 +6,20 @@ function WhatIDo(){
                 <p className= 'info'>I'm an experienced web-dev.</p>
                 <p className= 'info'>I use JS, React, Redux, and Axios.</p>
                 <div className='logosContainer'>
-                    <img className='jsLogo' src='images\js.png' />
-                    <img className='reactLogo' src='images\react.png' />
-                    <img className='reduxLogo' src='images\redux.png' />
-                    <img className='axiosLogo' src='images\axios.png' />
+                    <img className='jsLogo' src='public\js.png' />
+                    <img className='reactLogo' src='public\react.png' />
+                    <img className='reduxLogo' src='public\redux.png' />
+                    <img className='axiosLogo' src='public\axios.png' />
                 </div>
                 <span className='verticalSpacer' />
                 <h2 className='sectionTitle'>I build Backends.</h2>
                 <p className='info'>I have in-industry experience at ITS, Inc.</p>
                 <p className='info'>as a backend dev using Java, Kotlin, Spring, and MySQL.</p>
                 <div className='logosContainer'>
-                    <img className='leftLogo' src='images\java.png' />
-                    <img className='leftLogo' src='images\kotlin.png' />
-                    <img className='rightLogo' src='images\spring.png' />
-                    <img className='rightLogo' src='images\mysql.png' />
+                    <img className='leftLogo' src='public\java.png' />
+                    <img className='leftLogo' src='public\kotlin.png' />
+                    <img className='rightLogo' src='public\spring.png' />
+                    <img className='rightLogo' src='public\mysql.png' />
                 </div>
 
                 <h2 className='sectionTitle'>I build Mobile Apps.</h2>
